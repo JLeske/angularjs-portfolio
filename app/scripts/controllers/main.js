5995b7f6fd6f348585c42d231ced949b18a59fc7
JLeske <jens.leske@gmx.de>
2015-02-10 18:27:30 +0100

    Added a filter.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,20 +8,49 @@
  * Controller of the angularjsPortfolioApp
  */
 angular.module('angularjsPortfolioApp')
-  .controller('MainCtrl', ['$http', function ($http) {
-  	var self = this;
-  	self.works = [];
-    $http.get('./data/works.json')
-    	.success( function (data) {
-    		console.log(data);
-    		self.works = chunk(data, 3);
-    	});
-  }]);
+    .controller('MainCtrl', ['$http', function($http) {
+        var self = this,
+        	tmpData = [];
 
-function chunk(arr, size) {
-  var newArr = [];
-  for (var i=0; i < arr.length; i += size) {
-    newArr.push(arr.slice(i, i+size));
-  }
-  return newArr;
-}
+        self.works = [];
+        self.rawData = [];
+        $http.get('./data/works.json')
+            .success(function(data) {
+                console.log(data);
+                self.rawData = data;
+                
+                self.doFilter('all');
+            });
+
+        self.doFilter = function(type) {
+        	var obj = {};
+            console.log(type);
+            tmpData = [];
+            if ( type === 'all' )
+            {
+            	tmpData = self.rawData;
+            }
+            else
+            {
+            	for (var i = 0; i < self.rawData.length; i++) {
+            		
+            		obj = self.rawData[i];
+            		if ( obj.category_id == type )
+	            	{
+	            		tmpData.push(obj);
+	            	}
+            	}
+            }
+            
+            self.works = self.chunk(tmpData, 3);
+        };
+
+        self.chunk = function(arr, size) {
+            var newArr = [];
+            for (var i = 0; i < arr.length; i += size) {
+                newArr.push(arr.slice(i, i + size));
+            }
+            return newArr;
+        };
+    }]);
+
